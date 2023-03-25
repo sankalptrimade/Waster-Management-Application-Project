dc09c09a79bf1e189cf27f34d59cfd175cface37
sankalptrimade <sankalptrimade0014@gmail.com>
2023-03-25 13:31:15 +0530

    first commit

---
+++ src/main/java/com/project/FoodWasteManagementProject/FoodWasteManagementProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.FoodWasteManagementProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodWasteManagementProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodWasteManagementProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/FoodWasteManagementProject/FoodWasteManagementProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.FoodWasteManagementProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodWasteManagementProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
